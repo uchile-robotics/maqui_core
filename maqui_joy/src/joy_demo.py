940f47d9687cf2386ae7b229ff1edb5b8bb2b3c8
Lukas Pavez <lukas.pavez@ug.uchile.cl>
2018-01-26 16:59:47 -0300

    :dog: new joystick demo configuration :dog:

--- maqui_joy/src/joy_demo.py
+++ maqui_joy/src/joy_demo.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = 'Matias Pavez'
-__email__ = 'matias.pavez.b@gmail.com'
+
+__author__ = 'Lukas Pavez'
 
 import rospy
-import sys
 import math
 from sensor_msgs.msg import Joy
 from maqui_joy import xbox
 from maqui_skills import robot_factory
-from maqui_skills.core.tts import TTSSkill
-
-import rospy
-import actionlib
-
-from std_msgs.msg import( String )
-from std_srvs.srv import( Empty, EmptyResponse )
-
-from naoqi_bridge_msgs.msg import(
-    SpeechWithFeedbackGoal,
-    SpeechWithFeedbackResult,
-    SpeechWithFeedbackFeedback,
-    SpeechWithFeedbackAction )
 
 
-class JoystickTTS(object):
+class Joystick(object):
   
     def __init__(self,robot):
-        rospy.loginfo('Joystick TTS init ...')
+        rospy.loginfo('Joystick Joy demo init ...')
         
         self.robot = robot
         self.robot.tts.set_language("Spanish")
@@ -41,7 +27,11 @@ class JoystickTTS(object):
         b_decrement = rospy.get_param('~b_decrement', 'DOWN')
         b_phrase_1  = rospy.get_param('~b_phrase_1', 'LEFT')
         b_phrase_2  = rospy.get_param('~b_phrase_2', 'RIGHT')
-        
+        b_head      = rospy.get_param('~b_head', 'A')
+        b_tablet_image = rospy.get_param('~b_tablet_image', 'B')
+        b_tablet_reset = rospy.get_param('~b_tablet_reset', 'LB')
+        b_record = rospy.get_param('~b_record', 'X')
+        b_photo = rospy.get_param('~b_photo', 'Y')
 
         key_mapper = xbox.KeyMapper()
         self.b_idx_pause = key_mapper.get_button_id(self.b_pause)
@@ -49,19 +39,25 @@ class JoystickTTS(object):
         self.b_idx_decrement = key_mapper.get_button_id(b_decrement)
         self.b_idx_phrase_1  = key_mapper.get_button_id(b_phrase_1)
         self.b_idx_phrase_2  = key_mapper.get_button_id(b_phrase_2)
-
+        self.b_idx_head      = key_mapper.get_button_id(b_head)
+        self.b_idx_tablet_image = key_mapper.get_button_id(b_tablet_image)
+        self.b_idx_tablet_reset = key_mapper.get_button_id(b_tablet_reset)
+        self.b_idx_record = key_mapper.get_button_id(b_record)
+        self.b_idx_photo = key_mapper.get_button_id(b_photo)
         
         # check
         self.assert_params()
 
 
         # control
-        self.is_paused = False
-        self.tts_level = 0
-
+        self.is_recording = False
+        self.head_on      = False
+        self.is_paused    = False
+        self.tts_level    = 0
+        self.robot.media_camera.set_picture_params(resolution="2560x1920")
         # ready to work
         rospy.Subscriber('joy', Joy, self.callback, queue_size=1)
-        rospy.loginfo('Joystick for TTS is ready')
+        rospy.loginfo('Joystick for Joy is ready')
 
 
     def assert_params(self):
@@ -73,49 +69,15 @@ class JoystickTTS(object):
         assert isinstance(self.b_idx_decrement, int)
         assert isinstance(self.b_idx_phrase_1, int)
         assert isinstance(self.b_idx_phrase_2, int)
+        assert isinstance(self.b_idx_head, int)
+        assert isinstance(self.b_idx_tablet_image, int)
+        assert isinstance(self.b_idx_tablet_reset, int)
+        assert isinstance(self.b_idx_record, int)
+        assert isinstance(self.b_idx_photo, int)
 
         # check config
         if not len(self.tts_phrases):
             rospy.logwarn("Using Zero phrases for tts!")
-
-
-    def plugin(self):
-        pass
-        #     void  Joystick::synthesize(std::string text){
-        #     uchile_srvs::String speech_text;
-        #     std::string talk = text;
-        #     std::string text_evaluate = text;
-        #
-        #     std::size_t found_enter,found_emotion;
-        #     bool end = false;
-        #     std::string emo;
-        #     while(!end){
-        #         found_enter = text_evaluate.find("//");//Enter
-        #         found_emotion = text_evaluate.find("@");//Enter
-        #         talk = text_evaluate;
-        #         if (found_enter!=std::string::npos && found_enter<found_emotion){
-        #             talk =  text_evaluate.substr (0,found_enter);
-        #             text_evaluate = text_evaluate.substr (found_enter+3);
-        #         }
-        #         if (found_emotion!=std::string::npos && found_emotion<found_enter){
-        #             talk =  text_evaluate.substr (0,found_emotion);
-        #
-        #             std::size_t found_space = text_evaluate.find(" ",found_emotion);
-        #             if (found_space!=std::string::npos){
-        #                 emo = text_evaluate.substr (found_emotion+1,found_space - found_emotion -1);
-        #                 text_evaluate = text_evaluate.substr (found_space);
-        #             }else{
-        #                 emo = text_evaluate.substr (found_emotion+1);
-        #                 text_evaluate = text_evaluate.substr (found_emotion);
-        #                 end=true;
-        #             }
-        #             show_emotion(emo);
-        #         }
-        #         if (found_enter==std::string::npos && found_emotion==std::string::npos ) end=true;
-        #         speech_text.request.data = talk;
-        #         speech_serv_.call(speech_text);
-        #     }
-        # }
 
 
     def synthesize(self, text):
@@ -127,6 +89,36 @@ class JoystickTTS(object):
         except Exception, e:
             rospy.logwarn("TTS server failed!")
 
+    def head(self):
+        if not self.head_on:
+            self.robot.basic_awareness.look_around()
+        else:
+            self.robot.basic_awareness.stop()
+            self.robot.basic_motion.set_posture()
+        self.head_on = not self.head_on
+
+    def tablet_image(self):
+        rospy.loginfo("Showing amtc logo in tablet")
+        self.robot.tablet.show_image("http://198.18.0.1/apps/media/amtc_png.png","#FFFFFF")
+
+    def reset_tablet(self):
+        rospy.loginfo("Resetting tablet")
+        self.robot.tablet.sleep()
+        self.robot.tablet.wakeUp()
+        self.robot.tablet._resetTablet()
+
+    def record(self):
+        if not self.is_recording:
+            rospy.loginfo("Start video recording")
+            self.robot.media_camera.start_video_record("Joystick")
+        else:
+            rospy.loginfo("Stop video recording")
+            self.robot.media_camera.stop_video_record()
+        self.is_recording = not self.is_recording
+
+    def photo(self):
+        rospy.loginfo("Taking photos")
+        self.robot.media_camera.take_pictures("Joystick")
 
     def callback(self, msg):
 
@@ -183,12 +175,28 @@ class JoystickTTS(object):
                 if n_phrases > 0:
                     phrase_idx = (self.tts_level*2 + 1) % n_phrases
                     text = self.tts_phrases[phrase_idx]
-                    self.synthesize(text)      
+                    self.synthesize(text)
+
+            # start head
+            elif msg.buttons[self.b_idx_head]:
+                self.head()
+
+            elif msg.buttons[self.b_idx_tablet_image]:
+                self.tablet_image()
+
+            elif msg.buttons[self.b_idx_tablet_reset]:
+                self.reset_tablet()
+
+            elif msg.buttons[self.b_idx_record]:
+                self.record()
+
+            elif msg.buttons[self.b_idx_photo]:
+                self.photo()
+
             return
 
 if __name__ == '__main__':
-    rospy.init_node('joy_tts')
-    
-    robot = robot_factory.build(["tts","basic_awareness","basic_motion"], core=False)
-    JoystickTTS(robot)
+    rospy.init_node('joy')
+    robot = robot_factory.build(["tts","basic_awareness","basic_motion","media_camera","tablet"], core=False)
+    Joystick(robot)
     rospy.spin()
